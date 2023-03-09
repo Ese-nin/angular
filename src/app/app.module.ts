eb5e356e0789f80c9f595fe37e0e990838f344e1
Сергей <privet15.89@mail.ru>
2023-03-09 21:04:35 +0500

    *ngFor, *ngIf, [ngClass]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,13 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component'
-import { Test1Component } from './test1/test1.component'
-import { Test2Component } from './test2/test2.component'
-import { Test3Component } from './test3/test3.component'
-import { Test4Component } from './user/components/test4/test4.component'
-import { Test5Component } from './user/components/test5/test5.component'
-import { Test6Component } from './user/components/test6/test6.component'
+import { FormsModule } from '@angular/forms'
+import { ParentComponent } from './parent/parent.component'
+import { ChildrenComponent } from './parent/children/children.component'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    Test1Component,
-    Test2Component,
-    Test3Component,
-    Test4Component,
-    Test5Component,
-    Test6Component,
-  ],
-  imports: [BrowserModule],
+  declarations: [AppComponent, ParentComponent, ChildrenComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/parent/children/children.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Output } from '@angular/core'
+
+@Component({
+  selector: 'inst-children',
+  templateUrl: './children.component.html',
+  styleUrls: ['./children.component.css'],
+})
+export class ChildrenComponent {
+  @Output() sendGradeEvent = new EventEmitter<string>()
+
+  inputGrade = ''
+
+  sendGradeHandler() {
+    this.sendGradeEvent.emit(this.inputGrade)
+    this.inputGrade = ''
+  }
+}
